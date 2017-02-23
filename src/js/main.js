3e8167232fc545f81a7697e2c7000696b4eceed9
serii <seriiburduja@gmail.com>
2017-02-23 20:15:48 +0200

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,21 @@
+/*jquery*/
+//= ../../bower_components/jquery/dist/jquery.min.js
+
+/*jquery-migrate*/
+//= ../../bower_components/jquery-migrate/jquery-migrate.min.js
+
+/*smooth-scroll*/
+/*https://github.com/galambalazs/smoothscroll-for-websites*/
+//= ../../bower_components\smoothscroll-for-websites\SmoothScroll.js
+
+/*jquery-hover-delay*/
+/* https://github.com/john-terenzio/jQuery-Hover-Delay */
+//= ../../bower_components\jQuery-Hover-Delay\jquery.hoverdelay.min.js
+
+
+
+
+
+
+/*custom.js*/
+//= custom/custom.js
