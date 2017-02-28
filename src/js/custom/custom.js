5e19ef027942d1d60a1df8c95a6d1d41b547fb53
serii <seriiburduja@gmail.com>
2017-02-28 15:56:26 +0200

    finish no-bg slider

--- src/js/custom/custom.js
+++ src/js/custom/custom.js
@@ -53,4 +53,14 @@ $(function () {
 						$(rangeV).text(value + '%');
 				}
 		});
+
+		/*js-no-bg-slider*/
+		$('#js-no-bg-slider').slick({
+				dots: true,
+				fade: true,
+				speed: 3000,
+				autoplay: true,
+				autoplaySpeed: 20000,
+				arrows: false
+		});
 });
